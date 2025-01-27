15b6c891777944e0f7c64e14b0b0548fbbf37b6d
souravkanwar <souravkanwar007@gmail.com>
2025-01-27 15:28:57 +0530

    data

--- src/Components/Destination/Destination.jsx
+++ src/Components/Destination/Destination.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {destinations} from '../../Data'
+import React from "react";
+import { destinations } from "../../data";
 
 export default function Destination() {
   return (
@@ -7,17 +7,20 @@ export default function Destination() {
       <div className="destinations-container">
         <h1 className="destinations-header">Best Destination</h1>
         <div className="destinations">
-            {destinations.map((destination) =>(
-<div key={destination.id} className='destinations-item'>
-    <img src={destination.image} alt="" className='destinations-img'/>
-    <div className="destinations-title">
-        <h1>{destination.title}</h1>
-    </div>
-</div>
-
-            ))}
+          {destinations.map((destination) => (
+            <div key={destination.id} className="destinations-item">
+              <img
+                src={destination.image}
+                alt=""
+                className="destinations-img"
+              />
+              <div className="destinations-title">
+                <h1>{destination.title}</h1>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
-  )
+  );
 }
--- src/Components/Hotel/BestHotels.jsx
+++ src/Components/Hotel/BestHotels.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import Hotels from './Hotels'
-import { hotels } from '../../Data'
+import React from "react";
+import Hotels from "./Hotels";
+import { hotels } from "../../data";
 export default function BestHotels() {
   return (
     <>
-      <Hotels hotels={hotels} heading="Best Hotels"/>
+      <Hotels hotels={hotels} heading="Best Hotels" />
     </>
-  )
+  );
 }
